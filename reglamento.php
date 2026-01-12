e408fcf0cf6bf485e5474fdecca98751e6b4f0b5
Alejandro Aquino <68015154+Alesito199@users.noreply.github.com>
2026-01-12 10:39:36 -0300

    Archivos de configuración inicial
    Se agregaron los archivos de configuración inicial: package.json, package-lock.json, tailwind.config.js y reglamento.php. Se configuraron las dependencias de Tailwind CSS y Flowbite para los componentes de estilo e interfaz de usuario.

---
+++ reglamento.php
@@ -0,0 +1,25 @@
+<?php
+include "vistas/header/header-index.php";
+?>
+
+
+
+<div class="container mx-auto p-8">
+    <h2 class="text-4xl font-extrabold border-b text-blue-800 dark:text-white p-2 text-center justify-center">Reglamento General de la Universidad del Norte</h2>
+    <div class="mt-14">
+        <!-- Código para mostrar una vista previa del PDF -->
+        <object data="https://uninorte.edu.py/wp-content/uploads/2022/10/reglamento-general-de-la-universidad-del-norte-2021.pdf" type="application/pdf" width="100%" height="600px">
+            <p>No se puede mostrar la vista previa del PDF. <a href="https://uninorte.edu.py/wp-content/uploads/2022/10/reglamento-general-de-la-universidad-del-norte-2021.pdf" target="_blank">Haz clic aquí</a> para abrirlo.</p>
+        </object>
+
+        <!-- Enlace para abrir la vista previa del PDF -->
+
+        <a href="https://uninorte.edu.py/wp-content/uploads/2022/10/reglamento-general-de-la-universidad-del-norte-2021.pdf" class="absolute inline-flex items-end justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-blue-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+           
+        </a>
+    </div>
+</div>
+
+<?php
+include "vistas/footer/footer-index.php";
+?>
